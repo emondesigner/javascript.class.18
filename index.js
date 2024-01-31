177b953b32a07ad0557a9c8c4f1c9fb356e5d97a
emondesigner <152256898+emondesigner@users.noreply.github.com>
2024-01-31 11:25:32 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let signUpBtn = document.querySelector('.signUpBtn');
+let signInBtn = document.querySelector('.signInBtn');
+let tittle = document.querySelector('.tittle');
+
+
+
+signUpBtn.click =function(){
+    tittle.innerText ="Sign Up";
+
+
+}
+signInBtn.click =function(){
+    tittle.innerHTML = "Sing In";
+}
+
+
